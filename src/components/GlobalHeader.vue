30acce249985c91298a666e2ca06a310aa11cfaa
Prince <2596818595@qq.com>
2025-04-15 00:22:03 +0800

    🎉 init: first init

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="globalHeader">
+    <a-menu mode="horizontal" :default-selected-keys="['1']">
+      <a-menu-item
+        key="0"
+        :style="{ padding: 0, marginRight: '38px' }"
+        disabled
+      >
+        <div class="title-bar">
+          <img class="logo" src="@/assets/logo.png" />
+          <div class="title">AlgoCat</div>
+        </div>
+      </a-menu-item>
+      <a-menu-item v-for="item in routes" :key="item.path">{{
+        item.name
+      }}</a-menu-item>
+    </a-menu>
+  </div>
+</template>
+
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.logo {
+  width: 48px;
+}
+
+.title {
+  color: #444;
+  margin-left: 16px;
+}
+</style>
+
+<script setup>
+import { routes } from "@/router/routes.ts";
+</script>
---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header"><GlobalHeader /></a-layout-header>
+      <a-layout-content class="content"><router-view /></a-layout-content>
+      <a-layout-footer class="footer">AlgoCat by 骚客</a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scoped>
+.content {
+  text-align: center;
+}
+
+.footer {
+  background-color: #42b983;
+  text-align: center;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+</style>
+
+<script setup>
+import GlobalHeader from "@/components/GlobalHeader.vue";
+</script>
